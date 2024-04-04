ae117f0176334665c4ae01da1197813e3f344f8a
Laura <laura.e.vollmer@gmail.com>
2024-04-04 14:10:36 -0500

    adding design font to tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,11 @@ module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   darkMode: false, // or 'media' or 'class'
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'josefin': ['Josefin Sans', 'sans-serif'],
+      },
+    },
   },
   variants: {
     extend: {},
