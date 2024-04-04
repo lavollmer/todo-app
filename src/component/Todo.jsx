35a526ecb9b2a4123f8aaae7352615b0757b8e22
Laura <laura.e.vollmer@gmail.com>
2024-04-04 14:39:14 -0500

    added comments to code

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,6 +1,8 @@
-import React, { useState } from "react";
+import React, { useState } from "react"; // importing dependencies from react
 
+//creating the Todo function as a React functional component
 const Todo = () => {
+  // Setting up state
   // todos is an array of objects and each object represents a todo item
   // each todo has a unique id, text and isCompleted property
   //use setTodos to add, remove or update todos in your application
@@ -10,21 +12,29 @@ const Todo = () => {
     { id: 3, text: "Build a project", isCompleted: false },
   ]);
 
+  //inputValue represents the current value of the input field
   const [inputValue, setInputValue] = useState("");
 
+  //addTodo function is used to add a new todo item to the todos array
+  //addTodo is called when the form is submitted
   const addTodo = (e) => {
     //prevents the event from refreshing the page
     e.preventDefault();
+    //checks that inputValue is not an empty string
     if (inputValue.trim() !== "") {
+        //adds a new todo item to the todos array
       setTodos([
         ...todos,
         { id: Date.now(), text: inputValue, isCompleted: false },
       ]);
+      //clears the Input field after submitting the form
       setInputValue("");
     }
   };
+
   return (
     <div>
+        {/* onSubmit prop is used to call the addTodo function when the form is submitted */}
       <form onSubmit={addTodo}>
         <input
           type="text"
