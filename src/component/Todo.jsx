66ca4c853afb0b87dfdbe17208f3cb241b465efd
Laura <laura.e.vollmer@gmail.com>
2024-04-04 14:34:51 -0500

    adding Todo functionality

--- src/component/Todo.jsx
+++ src/component/Todo.jsx
@@ -1,15 +1,40 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Todo = () => {
-    // todos is an array of objects and each object represents a todo item
-    // each todo has a unique id, text and isCompleted property
-    //use setTodos to add, remove or update todos in your application
+  // todos is an array of objects and each object represents a todo item
+  // each todo has a unique id, text and isCompleted property
+  //use setTodos to add, remove or update todos in your application
   const [todos, setTodos] = useState([
     { id: 1, text: "Learn React", isCompleted: false },
     { id: 2, text: "Learn Redux", isCompleted: false },
     { id: 3, text: "Build a project", isCompleted: false },
   ]);
-  return <div>Todo</div>;
+
+  const [inputValue, setInputValue] = useState("");
+
+  const addTodo = (e) => {
+    //prevents the event from refreshing the page
+    e.preventDefault();
+    if (inputValue.trim() !== "") {
+      setTodos([
+        ...todos,
+        { id: Date.now(), text: inputValue, isCompleted: false },
+      ]);
+      setInputValue("");
+    }
+  };
+  return (
+    <div>
+      <form onSubmit={addTodo}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <button type="submit">Add</button>
+      </form>
+    </div>
+  );
 };
 
 export default Todo;
