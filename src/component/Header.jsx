04153586cb52de98158bcf2819538c9c6b570d7b
Laura <laura.e.vollmer@gmail.com>
2024-04-29 09:35:04 -0500

    updating header

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,9 +1,15 @@
-import { Flex, Heading } from "@chakra-ui/react";
+import { Flex, Heading, Button,Image } from "@chakra-ui/react";
+
+import moonIcon from "../assets/icon/icon-moon.svg";
+import sunIcon from "../assets/icon/icon-sun.svg";
 
 const Header = () => {
   return (
     <Flex justifyContent={"space-between"}>
       <Heading as="h2" size="xl" color="white" letterSpacing={".5em"}>TODO</Heading>
+    <Button variant={"ghost"} onClick={}>
+        <Image/>
+    </Button>
     </Flex>
   );
 };
