4f5a14955311b1e6a30ca74644ed608cd08f2ea6
Laura <laura.e.vollmer@gmail.com>
2024-04-29 09:28:31 -0500

    adding Header component

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,26 @@ import { useColorMode, Box } from "@chakra-ui/react";
 import { useState } from "react";
 import lightBackgroundImage from "./assets/images/bg-desktop-light.jpg";
 import darkBackgroundImage from "./assets/images/bg-desktop-dark.jpg";
+import Header from "./component/Header";
 
 function App() {
   const { colorMode, toggleColorMode } = useColorMode();
 
-  return <>
-  <Box
-  backgroundImage={
-    colorMode === "light" ?
-    lightBackgroundImage :
-    darkBackgroundImage
-  }
-  backgroundSize={"cover"}
-  h={"60vh"}
-  
-  >
-
-
-  </Box>
-
-  </>;
+  return (
+    <>
+      <Box
+        backgroundImage={
+          colorMode === "light" ? lightBackgroundImage : darkBackgroundImage
+        }
+        backgroundSize={"cover"}
+        h={"40vh"}
+      >
+        <Box w="40%" p="4em 0" m="auto">
+          <Header/>
+        </Box>
+      </Box>
+    </>
+  );
 }
 
 export default App;
