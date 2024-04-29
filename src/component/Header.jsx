d3f14e2df3edafce3ce341f815659b8a7193f189
Laura <laura.e.vollmer@gmail.com>
2024-04-29 09:45:57 -0500

    light and dark mode with button

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,15 +1,22 @@
-import { Flex, Heading, Button,Image } from "@chakra-ui/react";
+import { Flex, Heading, Button, Image } from "@chakra-ui/react";
 
 import moonIcon from "../assets/icon/icon-moon.svg";
 import sunIcon from "../assets/icon/icon-sun.svg";
 
-const Header = ({colorMode, toggleColorMode}) => {
+//functional component that receives two props colorMode and toggleColorMode
+const Header = ({ colorMode, toggleColorMode }) => {
   return (
+    //Flex is a flexible box layout from Chakra UI that allows alignment of child elements
     <Flex justifyContent={"space-between"}>
-      <Heading as="h2" size="xl" color="white" letterSpacing={".5em"}>TODO</Heading>
-    <Button variant={"ghost"} onClick={toggleColorMode}>
-        <Image src={colorMode === "light" ? moonIcon : sunIcon}/>
-    </Button>
+      {/* Heading component is used to display the title, as prop to specify the HTML elelement */}
+      <Heading as="h2" size="xl" color="white" letterSpacing={".5em"}>
+        TODO
+      </Heading>
+      {/* Button component is used to create a button that toggles color mode */}
+      <Button variant={"ghost"} onClick={toggleColorMode}>
+        {/* Image component is used to display an icon inside the button */}
+        <Image src={colorMode === "light" ? moonIcon : sunIcon} />
+      </Button>
     </Flex>
   );
 };
