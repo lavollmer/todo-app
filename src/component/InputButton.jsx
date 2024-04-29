a09bb1275b814f83277ebed0a5d1a120ce015919
Laura <laura.e.vollmer@gmail.com>
2024-04-29 09:59:00 -0500

    inputbutton debugging

--- src/component/InputButton.jsx
+++ src/component/InputButton.jsx
@@ -1,11 +1,23 @@
-import { Flex } from "@chakra-ui/react";
+import { Flex, InputGroup, InputLeftElement } from "@chakra-ui/react";
 
 const InputButton = ({ colorMode }) => {
   return (
     <Flex
       m="2.5em 0"
       background={colorMode === "light" ? "white" : "#1a202c"}
-    ></Flex>
+      p="2"
+      borderRadius={"0.5em"}
+    >
+      <InputGroup>
+        <InputLeftElement
+          pointerEvents={"none"}
+          display={"flex"}
+          justifyContent={"center"}
+          alignItems={"center"}
+          h={"100%"}
+        ></InputLeftElement>
+      </InputGroup>
+    </Flex>
   );
 };
 
