9349d462d5eadbd8dd07bfc6defe31e7392c3b43
Laura <laura.e.vollmer@gmail.com>
2024-04-29 09:18:26 -0500

    debugging switch to Chakra UI

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import "./App.css";
 import Todo from "./component/Todo";
 import Background from "./component/Background";
+import { useColorMode } from "@chakra-ui/react";
 
 function App() {
 const {colorMode, toggleColorMode} = useColorMode();
