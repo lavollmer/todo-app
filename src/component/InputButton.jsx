38ef64fc9c1d00eecf83cca5a4de06eb75b55d7c
Laura <laura.e.vollmer@gmail.com>
2024-04-29 10:11:11 -0500

    adding placeholder text and input

--- src/component/InputButton.jsx
+++ src/component/InputButton.jsx
@@ -1,5 +1,5 @@
-import { Flex, InputGroup, InputLeftElement } from "@chakra-ui/react";
-import Circle from "./Circle"
+import { Flex, InputGroup, InputLeftElement, Input } from "@chakra-ui/react";
+import Circle from "./Circle";
 
 const InputButton = ({ colorMode }) => {
   return (
@@ -19,6 +19,14 @@ const InputButton = ({ colorMode }) => {
         >
           <Circle width={"22px"} height={"22px"} />
         </InputLeftElement>
+        <Input
+          fontWeight={"700"}
+          fontSize={"1.2em"}
+          type="text"
+          h="3em"
+          variant={"unstyled"}
+          placeholder="Enter your todo help ..."
+        />
       </InputGroup>
     </Flex>
   );
